edc074fb9d6a5e3d2cfb15f52999f0cf247c6cd1
CalmDownVal <vaclav.lahuta@gmail.com>
2021-12-30 21:27:18 +0100

    improve resolution algorithm

--- src/resolver.ts
+++ src/resolver.ts
@@ -1,31 +1,28 @@
 import { dirname } from 'path';
 
-import { isCore, sync, SyncOpts } from 'resolve';
+import { isCore, sync as resolveSync, SyncOpts } from 'resolve';
 
 import { getMappedPath } from './mapping';
-import type { ResolverResult } from './types';
+import type { ResolverConfig, ResolverResult } from './types';
 import { log } from './utils/debug';
 import { defaultConfig, defaultExtensions, defaultPackageFilter } from './utils/defaults';
 import { getTypesPackageName, hasJSExtension, removeJSExtension, removeQueryString } from './utils/paths';
 
-function tsResolve(originalPath: string, options?: SyncOpts): string {
+function nodeResolve(originalPath: string, options?: SyncOpts): string {
 	try {
-		return sync(originalPath, options);
+		return resolveSync(originalPath, options);
 	}
 	catch (ex) {
 		const pathWithoutExt = removeJSExtension(originalPath);
 		if (pathWithoutExt !== originalPath) {
-			return sync(pathWithoutExt, options);
+			return resolveSync(pathWithoutExt, options);
 		}
 
 		throw ex;
 	}
 }
 
-export function resolve(source: string, file: string, config = defaultConfig): ResolverResult {
-	log('Looking for:', source);
-
-	const originalPath = removeQueryString(source);
+function resolveOne(originalPath: string, file: string, config: ResolverConfig): ResolverResult {
 	if (isCore(originalPath)) {
 		log('Matched as core module:', originalPath);
 		return {
@@ -39,41 +36,46 @@ export function resolve(source: string, file: string, config = defaultConfig): R
 		log('Mapped TypeScript path:', mappedPath);
 	}
 
-	let absolutePath = null;
 	try {
-		absolutePath = tsResolve(mappedPath ?? originalPath, {
+		const absolutePath = nodeResolve(mappedPath ?? originalPath, {
 			basedir: dirname(file),
 			extensions: config.extensions ?? defaultExtensions,
-			packageFilter: config.packageFilter ?? defaultPackageFilter
+			includeCoreModules: false,
+			packageFilter: config.packageFilter ?? defaultPackageFilter,
+			preserveSymlinks: true
 		});
-	}
-	catch {
-		// intentionally ignored
-	}
 
-	// attempt to look for @types/* in case we resolved to a JS file or are forced to by config
-	const shouldTryTypes = absolutePath
-		? hasJSExtension(absolutePath)
-		: config.alwaysTryTypes ?? false;
-
-	if (shouldTryTypes) {
-		const typesPackageName = getTypesPackageName(originalPath);
-		if (typesPackageName) {
-			const definitelyTyped = resolve(typesPackageName, file, config);
-			if (definitelyTyped.found) {
-				return definitelyTyped;
-			}
-		}
-	}
-
-	if (absolutePath) {
 		log('Matched path:', absolutePath);
 		return {
 			found: true,
 			path: absolutePath
 		};
 	}
+	catch {
+		log('Could not find:', originalPath);
+		return { found: false };
+	}
+}
 
-	log('Could not find:', originalPath);
-	return { found: false };
+export function resolve(source: string, file: string, config = defaultConfig): ResolverResult {
+	log('Looking for:', source);
+	const originalPath = removeQueryString(source);
+	const result = resolveOne(originalPath, file, config);
+
+	// attempt to look for @types/* in case we resolved to a JS file or are forced to by config
+	const shouldTryTypes = result.path
+		? hasJSExtension(result.path)
+		: config.alwaysTryTypes ?? false;
+
+	if (shouldTryTypes) {
+		const typesPackageName = getTypesPackageName(originalPath);
+		if (typesPackageName) {
+			const definitelyTyped = resolveOne(typesPackageName, file, config);
+			if (definitelyTyped.found) {
+				return definitelyTyped;
+			}
+		}
+	}
+
+	return result;
 }
